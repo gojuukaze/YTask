9f76f25136f8ac13e659dce4025f4013c8a30bfe
leffss <348926676@qq.com>
2020-03-13 12:43:52 +0800

    add rabbitmqbroker, memcachebackend and mongobackend

---
+++ v2/brokers/rabbitmq.go
@@ -0,0 +1,60 @@
+package brokers
+
+import (
+	"github.com/gojuukaze/YTask/v2/drive"
+	"github.com/gojuukaze/YTask/v2/message"
+	"github.com/gojuukaze/YTask/v2/util/yjson"
+)
+
+type RabbitMqBroker struct {
+	client   *drive.RabbitMqClient
+	host     string
+	port     string
+	user	 string
+	password string
+	//poolSize int
+}
+
+func NewRabbitMqBroker(host, port, user, password string) RabbitMqBroker {
+	return RabbitMqBroker{
+		host:     host,
+		port:     port,
+		password: password,
+		user:	  user,
+		//poolSize: 0,
+	}
+}
+
+func (r *RabbitMqBroker) Activate() {
+	client := drive.NewRabbitMqClient(r.host, r.port, r.user, r.password)
+	r.client = &client
+}
+
+func (r *RabbitMqBroker) SetPoolSize(n int) {
+	//r.poolSize = n
+}
+func (r *RabbitMqBroker) GetPoolSize() int {
+	//return r.poolSize
+	return 0
+}
+
+func (r *RabbitMqBroker) Next(queryName string) (message.Message, error) {
+	var msg message.Message
+	value, err := r.client.Get(queryName)
+	if err != nil {
+		return msg, err
+	}
+
+	err = yjson.YJson.UnmarshalFromString(value, &msg)
+	return msg, err
+}
+
+func (r *RabbitMqBroker) Send(queryName string, msg message.Message) error {
+	b, err := yjson.YJson.Marshal(msg)
+
+	if err != nil {
+		return err
+	}
+	err = r.client.Set(queryName, b)
+	return err
+}
--- v2/go.mod
+++ v2/go.mod
@@ -3,6 +3,7 @@ module github.com/gojuukaze/YTask/v2
 go 1.12
 
 require (
+	github.com/bradfitz/gomemcache v0.0.0-20190913173617-a41fca850d0b
 	github.com/go-redis/redis v0.0.0-20190813142431-c5c4ad6a4cae
 	github.com/golang/protobuf v1.3.2 // indirect
 	github.com/google/uuid v1.1.1
@@ -10,11 +11,11 @@ require (
 	github.com/onsi/ginkgo v1.10.1 // indirect
 	github.com/onsi/gomega v1.7.0 // indirect
 	github.com/sirupsen/logrus v1.4.2
+	github.com/streadway/amqp v0.0.0-20200108173154-1c71cc93ed71
+	go.mongodb.org/mongo-driver v1.3.1
 	golang.org/x/crypto v0.0.0-20190829043050-9756ffdc2472 // indirect
 	golang.org/x/net v0.0.0-20190827160401-ba9fcec4b297 // indirect
 	golang.org/x/sys v0.0.0-20190830142957-1e83adbbebd0 // indirect
-	golang.org/x/text v0.3.2 // indirect
 	golang.org/x/tools v0.0.0-20190830223141-573d9926052a // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
-	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
